728c6c9d0b4f2880a8aff01380308eec1cb27ff6
iberiaz9 <alvarado.ja53@gmail.com>
2020-07-12 12:00:31 -0700

    Decorator notebook

---
+++ dprof.py
@@ -0,0 +1,7 @@
+def dearProf(m="Mr. JA: I hope you like my code ...\n"):
+    def new_line_d(fn):
+        def new_line(*args, **kwargs):
+            print(m)
+            return fn(*args, **kwargs)
+        return new_line
+    return new_line_d
